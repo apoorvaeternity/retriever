3cd7b4f95fb6098c42c31865e6f752e776fd52f7
Apoorva Pandey <apoorvapandey365@gmail.com>
2019-05-19 09:15:57 +0530

    Add metadata file to archive

--- retriever/lib/provenance.py
+++ retriever/lib/provenance.py
@@ -1,11 +1,14 @@
+import json
 import os
 import subprocess
 import sys
 from datetime import datetime, timezone
+from tempfile import NamedTemporaryFile
 from zipfile import ZipFile
 
 from retriever import datasets, download
 from retriever.lib.defaults import HOME_DIR, VERSION
+from retriever.lib.engine_tools import getmd5
 
 
 def package_details():
@@ -20,13 +23,14 @@ def package_details():
     return details
 
 
-def commit_info():
+def commit_info(dataset):
     """
     Generate info for a particular commit.
     """
     info = {}
     info['packages'] = package_details()
     info['time'] = datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+    info['md5'] = getmd5(os.path.join(HOME_DIR, 'raw_data', dataset.name), 'dir')
     return info
 
 
@@ -48,23 +52,31 @@ def commit(dataset, path=None, force_download=False, quiet=False):
     elif dataset.name in os.listdir(raw_dir):
         data_exists = True
     if data_exists:
-        for root, directory, files in os.walk(os.path.join(raw_dir, dataset.name)):
+        for root, _, files in os.walk(os.path.join(raw_dir, dataset.name)):
             for file in files:
                 paths_to_zip['raw_data'].append(os.path.join(root, file))
-
-        with ZipFile(os.path.join(path, dataset.name), 'w') as zipped:
+        info = commit_info(dataset)
+        with ZipFile(os.path.join(path, '{}-{}'.format(dataset.name, info['md5'][:7])), 'w') as zipped:
             zipped.write(paths_to_zip['script'],
                          os.path.join('script', os.path.basename(paths_to_zip['script'])))
             for data_file in paths_to_zip['raw_data']:
                 zipped.write(data_file, os.path.join(data_file.lstrip(raw_dir).rstrip(os.path.basename(data_file)),
                                                      os.path.basename(data_file)))
+            metadata_temp_file = NamedTemporaryFile()
+            with open(os.path.abspath(metadata_temp_file.name), 'w') as json_file:
+                json.dump(info, json_file, sort_keys=True, indent=4)
+            zipped.write(os.path.abspath(metadata_temp_file.name), 'metadata.json')
+            metadata_temp_file.close()
+
+
     else:
         if not quiet:
             print("Dataset unavailable in downloaded datasets.")
 
 
 if __name__ == '__main__':
-    print(commit_info())
+
     for dataset in datasets():
         if dataset.name == 'gentry-forest-transects':
+            print(commit_info(dataset))
             print(commit(dataset, path='/home/apoorva/Desktop', force_download=True, quiet=True))
