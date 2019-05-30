20aa09a2d0a37b722768cdf9f67d6b327538dec6
Apoorva Pandey <apoorvapandey365@gmail.com>
2019-05-30 22:06:26 +0530

    Add command line interface for commit

--- retriever/lib/provenance.py
+++ retriever/lib/provenance.py
@@ -13,8 +13,7 @@ from retriever.lib.engine_tools import getmd5
 
 
 def package_details():
-    details = {}
-    details['retriever'] = VERSION[1:]
+    details = {'retriever': VERSION[1:]}
     packages = subprocess.check_output([sys.executable, '-m', 'pip',
                                         'freeze', '--exclude-editable']).decode("utf-8").split('\n')
     for package in packages:
@@ -28,20 +27,21 @@ def commit_info(dataset):
     """
     Generate info for a particular commit.
     """
-    info = {}
-    info['packages'] = package_details()
-    info['time'] = datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
-    info['version'] = dataset.version
+    info = {'packages': package_details(),
+            'time': datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S"),
+            'version': dataset.version}
     return info
 
 
-def commit(dataset, path=None, quiet=False):
+def commit(dataset, commit_message='', path='.', quiet=False):
     """
     Commit dataset to a zipped file.
     """
-    paths_to_zip = {}
-    paths_to_zip['script'] = dataset._file
-    paths_to_zip['raw_data'] = []
+    if type(dataset) == str:
+        # if dataset is not a dataset script object find the right script
+        dataset = [script for script in datasets() if script.name == dataset][0]
+    paths_to_zip = {'script': dataset._file,
+                    'raw_data': []}
     raw_dir = os.path.join(HOME_DIR, 'raw_data')
     data_exists = False
     if not quiet:
@@ -61,11 +61,11 @@ def commit(dataset, path=None, quiet=False):
                     paths_to_zip['raw_data'].append(os.path.join(root, file))
 
             info = commit_info(dataset)
+            info['commit_message'] = commit_message
             info['script_name'] = os.path.basename(dataset._file)
 
             if os.path.exists(os.path.join(HOME_DIR, 'raw_data', dataset.name)):
                 info['md5'] = getmd5(os.path.join(HOME_DIR, 'raw_data', dataset.name), 'dir', encoding=ENCODING)
-
             with ZipFile(os.path.join(path, '{}-{}.zip'.format(dataset.name,
                                                                info['md5'][:7])), 'w') as zipped:
                 zipped.write(paths_to_zip['script'],
