7f4da09da204c5a03ac1df48f0b415698c6b94c9
Apoorva Pandey <apoorvapandey365@gmail.com>
2019-06-17 22:14:12 +0530

    Update urls to csv file

--- retriever/lib/provenance.py
+++ retriever/lib/provenance.py
@@ -14,7 +14,7 @@ def package_details():
     details = {}
     packages = pkg_resources.working_set
     for package in packages:
-        package_name, version = str(package).split(' ')
+        package_name, version = str(package).split(" ")
         details[package_name] = version
     return details
 
@@ -23,28 +23,29 @@ def commit_info(dataset):
     """
     Generate info for a particular commit.
     """
-    info = {'packages': package_details(),
-            'time': datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S"),
-            'version': dataset.version}
+    info = {
+        "packages": package_details(),
+        "time": datetime.now(timezone.utc).strftime("%m/%d/%Y, %H:%M:%S"),
+        "version": dataset.version,
+    }
     return info
 
 
-def commit(dataset, commit_message='', path='.', quiet=False):
+def commit(dataset, commit_message="", path=".", quiet=False):
     """
     Commit dataset to a zipped file.
     """
     if isinstance(dataset, str):
         # if dataset is not a dataset script object find the right script
         dataset = [script for script in datasets() if script.name == dataset][0]
-    paths_to_zip = {'script': dataset._file,
-                    'raw_data': []}
-    raw_dir = os.path.join(HOME_DIR, 'raw_data')
+    paths_to_zip = {"script": dataset._file, "raw_data": []}
+    raw_dir = os.path.join(HOME_DIR, "raw_data")
     data_exists = False
     if not quiet:
         print("Committing dataset {}".format(dataset.name))
     try:
         if dataset.name not in os.listdir(raw_dir):
-            engine = choose_engine({'command': 'download', 'path': './', 'sub_dir': ""})
+            engine = choose_engine({"command": "download", "path": "./", "sub_dir": ""})
             dataset.download(engine=engine, debug=quiet)
             data_exists = True
 
@@ -54,27 +55,41 @@ def commit(dataset, commit_message='', path='.', quiet=False):
         if data_exists:
             for root, _, files in os.walk(os.path.join(raw_dir, dataset.name)):
                 for file in files:
-                    paths_to_zip['raw_data'].append(os.path.join(root, file))
+                    paths_to_zip["raw_data"].append(os.path.join(root, file))
 
             info = commit_info(dataset)
-            info['commit_message'] = commit_message
-            info['script_name'] = os.path.basename(dataset._file)
+            info["commit_message"] = commit_message
+            info["script_name"] = os.path.basename(dataset._file)
 
-            if os.path.exists(os.path.join(HOME_DIR, 'raw_data', dataset.name)):
-                info['md5_dataset'] = getmd5(os.path.join(HOME_DIR, 'raw_data', dataset.name), 'dir', encoding=ENCODING)
-            info['md5_script'] = getmd5(dataset._file, data_type='file', encoding=ENCODING)
-            with ZipFile(os.path.join(path, '{}-{}{}.zip'.format(dataset.name,
-                                                                 info['md5_dataset'][:3],
-                                                                 info['md5_script'][:3])), 'w') as zipped:
-                zipped.write(paths_to_zip['script'],
-                             os.path.join('script', os.path.basename(paths_to_zip['script'])))
+            if os.path.exists(os.path.join(HOME_DIR, "raw_data", dataset.name)):
+                info["md5_dataset"] = getmd5(
+                    os.path.join(HOME_DIR, "raw_data", dataset.name),
+                    "dir",
+                    encoding=ENCODING,
+                )
+            info["md5_script"] = getmd5(
+                dataset._file, data_type="file", encoding=ENCODING
+            )
+            with ZipFile(
+                os.path.join(
+                    path,
+                    "{}-{}{}.zip".format(
+                        dataset.name, info["md5_dataset"][:3], info["md5_script"][:3]
+                    ),
+                ),
+                "w",
+            ) as zipped:
+                zipped.write(
+                    paths_to_zip["script"],
+                    os.path.join("script", os.path.basename(paths_to_zip["script"])),
+                )
 
-                for data_file in paths_to_zip['raw_data']:
+                for data_file in paths_to_zip["raw_data"]:
                     zipped.write(data_file, data_file.replace(raw_dir, ""))
-                with open('metadata.json', 'w') as json_file:
+                with open("metadata.json", "w") as json_file:
                     json.dump(info, json_file, sort_keys=True, indent=4)
-                zipped.write(os.path.abspath(json_file.name), 'metadata.json')
-                os.remove('metadata.json')
+                zipped.write(os.path.abspath(json_file.name), "metadata.json")
+                os.remove("metadata.json")
         if not quiet:
             print("Successfully committed.")
     except Exception as e:
--- test/test_provenance.py
+++ test/test_provenance.py
@@ -8,12 +8,22 @@ from retriever.lib.load_json import read_json
 from retriever.lib.provenance import commit
 
 file_location = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
-mysql_engine, postgres_engine, sqlite_engine, msaccess_engine, \
-csv_engine, download_engine, json_engine, xml_engine = engine_list
+mysql_engine, postgres_engine, sqlite_engine, msaccess_engine, csv_engine, download_engine, json_engine, xml_engine = (
+    engine_list
+)
 
-test_commit_details = [('dataset_provenance', {
-    'main': 'https://github.com/apoorvaeternity/sample-dataset/raw/master/modified/dataset_provenance.csv'},
-                        {'original': 'dataset-provenance-396e22.zip', 'modified': 'dataset-provenance-a69e22.zip'})]
+test_commit_details = [
+    (
+        "dataset_provenance",
+        {
+            "main": "https://raw.githubusercontent.com/weecology/retriever/master/test/raw_data/dataset-provenance/modified/dataset_provenance.csv"
+        },
+        {
+            "original": "dataset-provenance-396e22.zip",
+            "modified": "dataset-provenance-a69e22.zip",
+        },
+    )
+]
 
 
 def get_script_module(script_name):
@@ -24,9 +34,13 @@ def get_script_module(script_name):
 def install_and_commit(script_module, test_dir, commit_message, modified_table_urls={}):
     for table in modified_table_urls:
         # modify the url to the csv file
-        setattr(script_module.tables[table], 'url', modified_table_urls[table])
-    sqlite_engine.opts = {'install': 'sqlite', 'file': 'test_db.sqlite3', 'table_name': '{db}_{table}',
-                          'data_dir': '.'}
+        setattr(script_module.tables[table], "url", modified_table_urls[table])
+    sqlite_engine.opts = {
+        "install": "sqlite",
+        "file": "test_db.sqlite3",
+        "table_name": "{db}_{table}",
+        "data_dir": ".",
+    }
     sqlite_engine.use_cache = False
     # check if dataset is installing properly
     script_module.download(engine=sqlite_engine)
@@ -34,24 +48,43 @@ def install_and_commit(script_module, test_dir, commit_message, modified_table_u
     commit(script_module, path=test_dir, commit_message=commit_message)
 
 
-@pytest.mark.parametrize("script_file_name, modified_table_urls, expected_archives", test_commit_details)
+@pytest.mark.parametrize(
+    "script_file_name, modified_table_urls, expected_archives", test_commit_details
+)
 def test_commit(script_file_name, modified_table_urls, expected_archives):
     test_dir = mkdtemp(dir=os.path.dirname(os.path.realpath(__file__)))
     os.chdir(test_dir)
-    script_module = get_script_module(os.path.join('raw_data/scripts/', script_file_name))
-    setattr(script_module, "_file",
-            os.path.join(file_location, 'raw_data/scripts/', '{}.json'.format(script_file_name)))
-    setattr(script_module, "_name", script_file_name.replace('_', '-'))
+    script_module = get_script_module(
+        os.path.join("raw_data/scripts/", script_file_name)
+    )
+    setattr(
+        script_module,
+        "_file",
+        os.path.join(
+            file_location, "raw_data/scripts/", "{}.json".format(script_file_name)
+        ),
+    )
+    setattr(script_module, "_name", script_file_name.replace("_", "-"))
     # install original version
     install_and_commit(script_module, test_dir=test_dir, commit_message="Original")
     # install modified version
-    install_and_commit(script_module, test_dir=test_dir, commit_message="Modified",
-                       modified_table_urls=modified_table_urls)
+    install_and_commit(
+        script_module,
+        test_dir=test_dir,
+        commit_message="Modified",
+        modified_table_urls=modified_table_urls,
+    )
     # check if the required archive files exist
-    original_archive_exist = True if os.path.isfile(
-        os.path.join(test_dir, expected_archives['original'])) else False
-    modified_archive_exist = True if os.path.isfile(
-        os.path.join(test_dir, expected_archives['modified'])) else False
+    original_archive_exist = (
+        True
+        if os.path.isfile(os.path.join(test_dir, expected_archives["original"]))
+        else False
+    )
+    modified_archive_exist = (
+        True
+        if os.path.isfile(os.path.join(test_dir, expected_archives["modified"]))
+        else False
+    )
     os.chdir(file_location)
     rmtree(test_dir)
 
